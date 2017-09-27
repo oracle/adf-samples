fdd7d4801d1c4944a50470f16101c8529f77ba2c
Duncan Mills <duncan.mills@oracle.com>
2017-09-27 12:56:16 +0100

    ListBoundToSessionScopeBeanProperty

--- README.md
+++ README.md
@@ -120,6 +120,7 @@ Each sample is also available in downloadable form in the zips directory.
 * JSFOrdImageExample
 * JSPGraphInJSFPage
 * JTableWithDataDrivenCombo
+* ListBoundToSessionScopeBeanProperty
 
 
 
---
+++ src/ListBoundToSessionScopeBeanProperty/ViewController/src/test/view/MyBean.java
@@ -0,0 +1,16 @@
+/* Copyright 2010, 2017, Oracle and/or its affiliates. All rights reserved. */
+package test.view;
+
+import oracle.jbo.domain.Number;
+
+public class MyBean {
+    Number selectedDepartment;
+
+    public void setSelectedDepartment(Number selectedDepartment) {
+        this.selectedDepartment = selectedDepartment;
+    }
+
+    public Number getSelectedDepartment() {
+        return selectedDepartment;
+    }
+}
